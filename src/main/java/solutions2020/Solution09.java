3ef2d22c8ac73ca9cc0d9f2dc45e62927c4ef468
alessandrofranchini <alessandro.franchini@wolterskluwer.com>
2020-12-11 17:12:15 +0100

    Solution day 9: improved

--- src/main/java/solutions2020/Solution09.java
+++ src/main/java/solutions2020/Solution09.java
@@ -1,6 +1,5 @@
 package solutions2020;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -8,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -18,10 +18,14 @@ public class Solution09 {
   private static final int PREAMBLE_SIZE = 25;
 
   public static long partOne(Stream<String> lines) {
+    return getInvalidNumber(lines.map(Long::parseLong).collect(Collectors.toList()));
+  }
+
+  private static Long getInvalidNumber(List<Long> allNumber) {
     Map<Long, Boolean> seen = new HashMap<>();
     Queue<Long> queue = new LinkedList<>();
     AtomicInteger i = new AtomicInteger(1);
-    return lines.map(Long::parseLong).filter(number -> {
+    return allNumber.stream().filter(number -> {
       queue.add(number);
       if (i.get() <= PREAMBLE_SIZE) {
         seen.put(number, true);
@@ -40,45 +44,24 @@ public class Solution09 {
   }
 
   public static long partTwo(Stream<String> lines) {
-    Map<Long, Boolean> seen = new HashMap<>();
-    Queue<Long> queue = new LinkedList<>();
-    List<Long> allNumber = new ArrayList<>();
-    AtomicInteger i = new AtomicInteger(1);
-    Long invalidNumber = lines.map(Long::parseLong).filter(number -> {
-      allNumber.add(number);
-      queue.add(number);
-      if (i.get() <= PREAMBLE_SIZE) {
-        seen.put(number, true);
-        i.getAndIncrement();
-      } else {
-        boolean valid = queue.stream().anyMatch(previous -> Boolean.TRUE.equals(seen.get(Math.abs(number - previous))));
-        if (valid) {
-          seen.put(queue.poll(), false);
-          seen.put(number, true);
-        } else {
-          return true;
-        }
-      }
-      return false;
-    }).findFirst().get();
-    return getEncryptionWeakness(allNumber, invalidNumber);
+    List<Long> allNumber = lines.map(Long::parseLong).collect(Collectors.toList());
+    return getEncryptionWeakness(allNumber, getInvalidNumber(allNumber));
   }
 
   private static long getEncryptionWeakness(List<Long> allNumber, Long invalidNumber) {
-    long sum = allNumber.get(0);
-    int j;
-    int k = 1;
-    for (j = 0; j < allNumber.size() && sum != invalidNumber; j++) {
-      while (k < allNumber.size() && sum < invalidNumber) {
-        sum += allNumber.get(k);
-        if (sum == invalidNumber) {
-          break;
-        }
-        k++;
+    long sum = allNumber.get(0) + allNumber.get(1);
+    int left = 0;
+    int right = 2;
+    while (right < allNumber.size() && sum != invalidNumber) {
+      if (sum < invalidNumber) {
+        sum += allNumber.get(right);
+        right++;
+      } else {
+        sum -= allNumber.get(left);
+        left++;
       }
-      sum -= allNumber.get(j);
     }
-    List<Long> contiguosNumbers = allNumber.subList(j, k + 1);
+    List<Long> contiguosNumbers = allNumber.subList(left, right);
     contiguosNumbers.sort(Comparator.naturalOrder());
     return contiguosNumbers.get(0) + contiguosNumbers.get(contiguosNumbers.size() - 1);
   }
