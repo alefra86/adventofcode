35822905b2953e20dbc54916c46e9eebaef08872
alessandrofranchini <alessandro.franchini@wolterskluwer.com>
2020-12-11 17:17:47 +0100

    Solution day 10: improved

--- src/main/java/solutions2020/Solution10.java
+++ src/main/java/solutions2020/Solution10.java
@@ -12,29 +12,27 @@ import java.util.stream.Stream;
 public class Solution10 {
 
   public static int partOne(Stream<String> lines) {
-    List<Integer> adapters = lines.map(Integer::parseInt).sorted().collect(Collectors.toList());
+    List<Integer> adapters = getAdapters(lines);
     int differenceByOne = 0;
     int differenceByThree = 1;
-    int jolts = 0;
-    for (Integer adapter : adapters) {
-      if (adapter - jolts <= 3) {
-        if (adapter - jolts == 1) {
-          differenceByOne++;
-        } else if (adapter - jolts == 3) {
-          differenceByThree++;
-        }
-        jolts = adapter;
-      } else {
-        break;
+    for (int i = 1; i < adapters.size() && adapters.get(i) - adapters.get(i - 1) <= 3; i++) {
+      if (adapters.get(i) - adapters.get(i - 1) == 1) {
+        differenceByOne++;
+      } else if (adapters.get(i) - adapters.get(i - 1) == 3) {
+        differenceByThree++;
       }
     }
     return differenceByOne * differenceByThree;
   }
 
   public static long partTwo(Stream<String> lines) {
+    return distinctWays(0, getAdapters(lines), new HashMap<>());
+  }
+
+  private static List<Integer> getAdapters(Stream<String> lines) {
     List<Integer> adapters = lines.map(Integer::parseInt).sorted().collect(Collectors.toList());
     adapters.add(0, 0);
-    return distinctWays(0, adapters, new HashMap<>());
+    return adapters;
   }
 
   public static long distinctWays(int currentPosition, List<Integer> adapters, Map<Integer, Long> cache) {
@@ -49,8 +47,7 @@ public class Solution10 {
     int currentJolts = adapters.get(currentPosition);
     long ways = 0;
     while (i < adapters.size() && adapters.get(i) - currentJolts <= 3) {
-      ways += distinctWays(i, adapters, cache);
-      i++;
+      ways += distinctWays(i++, adapters, cache);
     }
     cache.put(currentPosition, ways);
     return ways;
