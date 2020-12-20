dd5babbd638355646fcf18d29151b8969f735b57
alessandrofranchini <alessandro.franchini@wolterskluwer.com>
2020-12-20 02:11:33 +0100

    Refactor solution day 18

--- java/src/main/java/solutions2020/Solution18.java
+++ java/src/main/java/solutions2020/Solution18.java
@@ -1,163 +1,300 @@
 package solutions2020;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import org.apache.commons.lang3.tuple.Pair;
+import solutions2020.Solution18.DefaultPrecedenceEvaluator.PrecedenceEvaluatorBuilder;
 
 /**
  *
  */
 public class Solution18 {
 
-  public static final String PARENTHESIS = "\\(([^()]+)\\)";
-  public static final String SUM = "(\\d+) \\+ (\\d+)";
+  public static class ParserCalculator {
 
-  enum Operator {
-    PLUS(Long::sum),
-    MULTIPLY((a, b) -> a * b);
-
-    private final BiFunction<Long, Long, Long> function;
-
-    Operator(BiFunction<Long, Long, Long> function) {
-      this.function = function;
+    public long partOne(List<String> lines) {
+      return lines.stream().map(expression -> resolveExpression(expression.replace(" ", ""), 0).getLeft()).reduce(0L, Long::sum);
     }
 
-    public BiFunction<Long, Long, Long> getFunction() {
-      return function;
+    public long partTwo(List<String> lines) {
+      return lines.stream()
+               .map(expression -> resolveExpressionPrecedence(expression.replace(" ", ""), 0).getLeft())
+               .reduce(0L, Long::sum);
     }
-  }
 
-  public static long partOne(List<String> lines) {
-    return lines.stream().map(expression -> resolve(expression, Function.identity())).reduce(0L, Long::sum);
-  }
-
-  public static long partTwo(List<String> lines) {
-    return lines.stream().map(expression -> resolve(expression, Solution18::resolveBeforeSum)).reduce(0L, Long::sum);
-  }
-
-  public static long partOneRec(List<String> lines) {
-    return lines.stream().map(expression -> resolveExpression(expression.replace(" ", ""), 0).getLeft()).reduce(0L, Long::sum);
-  }
-
-  public static long partTwoRec(List<String> lines) {
-    return lines.stream()
-             .map(expression -> resolveExpressionPrecedence(expression.replace(" ", ""), 0).getLeft())
-             .reduce(0L, Long::sum);
-  }
-
-  private static long eval(long first, char operation, long second) {
-    if (operation == '+') {
-      return first + second;
-    } else {
-      return first * second;
-    }
-  }
-
-  private static Pair<Long, Integer> resolveAdd(String expression, int position) {
-    Pair<Long, Integer> firstTerm = getTerm(expression, position, Solution18::resolveExpressionPrecedence);
-    long result = firstTerm.getLeft();
-    position = firstTerm.getRight();
-    while (isNotEndExpression(expression, position) && expression.charAt(position) != '*') {
-      Pair<Long, Integer> secondTerm = getTerm(expression, position + 1, Solution18::resolveExpressionPrecedence);
-      result += secondTerm.getLeft();
-      position = secondTerm.getRight();
-    }
-    return Pair.of(result, position);
-  }
-
-  private static Pair<Long, Integer> resolveMul(String expression, int position) {
-    Pair<Long, Integer> firstTerm = resolveAdd(expression, position);
-    long result = firstTerm.getLeft();
-    position = firstTerm.getRight();
-    while (isNotEndExpression(expression, position)) {
-      Pair<Long, Integer> secondTerm = resolveAdd(expression, position + 1);
-      result *= secondTerm.getLeft();
-      position = secondTerm.getRight();
-    }
-    return Pair.of(result, position);
-  }
-
-  private static Pair<Long, Integer> resolveExpressionPrecedence(String expression, int position) {
-    Pair<Long, Integer> firstTerm = resolveMul(expression, position);
-    long result = firstTerm.getLeft();
-    position = firstTerm.getRight();
-    while (isNotEndExpression(expression, position)) {
-      Pair<Long, Integer> secondTerm = resolveMul(expression, position + 1);
-      result = eval(result, expression.charAt(position), secondTerm.getLeft());
-      position = secondTerm.getRight();
-    }
-    return Pair.of(result, position);
-  }
-
-  private static Pair<Long, Integer> getTerm(String expression, int position,
-    BiFunction<String, Integer, Pair<Long, Integer>> expressionSolver) {
-    if (expression.charAt(position) == '(') {
-      Pair<Long, Integer> resolvedExpression = expressionSolver.apply(expression, position + 1);
-      return Pair.of(resolvedExpression.getLeft(), resolvedExpression.getRight() + 1);
-    } else {
-      return Pair.of((long) Character.getNumericValue(expression.charAt(position)), position + 1);
-    }
-  }
-
-  private static Pair<Long, Integer> resolveExpression(String expression, int position) {
-    Pair<Long, Integer> firstTerm = getTerm(expression, position, Solution18::resolveExpression);
-    long result = firstTerm.getLeft();
-    position = firstTerm.getRight();
-    while (isNotEndExpression(expression, position)) {
-      Pair<Long, Integer> secondTerm = getTerm(expression, position + 1, Solution18::resolveExpression);
-      result = eval(result, expression.charAt(position), secondTerm.getLeft());
-      position = secondTerm.getRight();
-    }
-    return Pair.of(result, position);
-  }
-
-  private static boolean isNotEndExpression(String expression, Integer position) {
-    return position != expression.length() && expression.charAt(position) != ')';
-  }
-
-  private static long resolve(String expression, Function<String, String> precedenceResolver) {
-    Matcher matcher = Pattern.compile(PARENTHESIS).matcher(expression);
-    while (matcher.find()) {
-      expression = expression.replaceFirst(PARENTHESIS, String.valueOf(resolve(precedenceResolver.apply(matcher.group(1)))));
-      matcher = Pattern.compile(PARENTHESIS).matcher(expression);
-    }
-    return resolve(precedenceResolver.apply(expression));
-  }
-
-  private static String resolveBeforeSum(String expression) {
-    Matcher matcher = Pattern.compile(SUM).matcher(expression);
-    while (matcher.find()) {
-      expression = expression.replaceFirst(SUM, Integer.parseInt(matcher.group(1)) + Integer.parseInt(matcher.group(2)) + "");
-      matcher = Pattern.compile(SUM).matcher(expression);
-    }
-    return expression;
-  }
-
-  private static long resolve(String expression) {
-    Stack<Long> stack = new Stack<>();
-    Stack<Operator> operation = new Stack<>();
-    String[] charArray = expression.split(" ");
-    for (String c : charArray) {
-      switch (c) {
-        case "+":
-          operation.push(Operator.PLUS);
-          break;
-        case "*":
-          operation.push(Operator.MULTIPLY);
-          break;
-        default:
-          stack.push(Long.valueOf(c));
-          if (stack.size() > 1) {
-            stack.push(operation.pop().getFunction().apply(stack.pop(), stack.pop()));
-          }
-          break;
+    private long eval(long first, char operation, long second) {
+      if (operation == '+') {
+        return first + second;
+      } else {
+        return first * second;
       }
     }
-    return stack.pop();
+
+    private Pair<Long, Integer> resolveAdd(String expression, int position) {
+      Pair<Long, Integer> firstTerm = getTerm(expression, position, this::resolveExpressionPrecedence);
+      long result = firstTerm.getLeft();
+      position = firstTerm.getRight();
+      while (isNotEndExpression(expression, position) && expression.charAt(position) != '*') {
+        Pair<Long, Integer> secondTerm = getTerm(expression, position + 1, this::resolveExpressionPrecedence);
+        result += secondTerm.getLeft();
+        position = secondTerm.getRight();
+      }
+      return Pair.of(result, position);
+    }
+
+    private Pair<Long, Integer> resolveMul(String expression, int position) {
+      Pair<Long, Integer> firstTerm = resolveAdd(expression, position);
+      long result = firstTerm.getLeft();
+      position = firstTerm.getRight();
+      while (isNotEndExpression(expression, position)) {
+        Pair<Long, Integer> secondTerm = resolveAdd(expression, position + 1);
+        result *= secondTerm.getLeft();
+        position = secondTerm.getRight();
+      }
+      return Pair.of(result, position);
+    }
+
+    private Pair<Long, Integer> resolveExpressionPrecedence(String expression, int position) {
+      Pair<Long, Integer> firstTerm = resolveMul(expression, position);
+      long result = firstTerm.getLeft();
+      position = firstTerm.getRight();
+      while (isNotEndExpression(expression, position)) {
+        Pair<Long, Integer> secondTerm = resolveMul(expression, position + 1);
+        result = eval(result, expression.charAt(position), secondTerm.getLeft());
+        position = secondTerm.getRight();
+      }
+      return Pair.of(result, position);
+    }
+
+    private Pair<Long, Integer> getTerm(String expression, int position,
+      BiFunction<String, Integer, Pair<Long, Integer>> expressionSolver) {
+      if (expression.charAt(position) == '(') {
+        Pair<Long, Integer> resolvedExpression = expressionSolver.apply(expression, position + 1);
+        return Pair.of(resolvedExpression.getLeft(), resolvedExpression.getRight() + 1);
+      } else {
+        return Pair.of((long) Character.getNumericValue(expression.charAt(position)), position + 1);
+      }
+    }
+
+    private Pair<Long, Integer> resolveExpression(String expression, int position) {
+      Pair<Long, Integer> firstTerm = getTerm(expression, position, this::resolveExpression);
+      long result = firstTerm.getLeft();
+      position = firstTerm.getRight();
+      while (isNotEndExpression(expression, position)) {
+        Pair<Long, Integer> secondTerm = getTerm(expression, position + 1, this::resolveExpression);
+        result = eval(result, expression.charAt(position), secondTerm.getLeft());
+        position = secondTerm.getRight();
+      }
+      return Pair.of(result, position);
+    }
+
+    private boolean isNotEndExpression(String expression, Integer position) {
+      return position != expression.length() && expression.charAt(position) != ')';
+    }
+  }
+
+  public static class PatternMatchingCalculator {
+
+    private static final String PARENTHESIS = "\\(([^()]+)\\)";
+    private static final String SUM = "(\\d+) \\+ (\\d+)";
+
+    public PatternMatchingCalculator() {
+    }
+
+    public long partOne(List<String> lines) {
+      return lines.stream().map(expression -> resolve(expression, Function.identity())).reduce(0L, Long::sum);
+    }
+
+    public long partTwo(List<String> lines) {
+      return lines.stream().map(expression -> resolve(expression, this::resolveBeforeSum)).reduce(0L, Long::sum);
+    }
+
+    private long resolve(String expression, Function<String, String> precedenceResolver) {
+      Matcher matcher = Pattern.compile(PARENTHESIS).matcher(expression);
+      while (matcher.find()) {
+        expression = expression.replaceFirst(PARENTHESIS, String.valueOf(resolve(precedenceResolver.apply(matcher.group(1)))));
+        matcher = Pattern.compile(PARENTHESIS).matcher(expression);
+      }
+      return resolve(precedenceResolver.apply(expression));
+    }
+
+    private long resolve(String expression) {
+      return (long) new Calculator(new PrecedenceEvaluatorBuilder().withOperatorPrecedence(Operator.PLUS, 1)
+                                     .withOperatorPrecedence(Operator.MULTIPLY, 1)
+                                     .withOperatorPrecedence(Operator.LP, 4)
+                                     .withOperatorPrecedence(Operator.RP, 4)
+                                     .build()).evaluate(expression);
+    }
+
+    private String resolveBeforeSum(String expression) {
+      Matcher matcher = Pattern.compile(SUM).matcher(expression);
+      while (matcher.find()) {
+        expression = expression.replaceFirst(SUM, Integer.parseInt(matcher.group(1)) + Integer.parseInt(matcher.group(2)) + "");
+        matcher = Pattern.compile(SUM).matcher(expression);
+      }
+      return expression;
+    }
+
+  }
+
+  interface PrecedenceEvaluator extends Comparator<Operator> {
+
+  }
+
+  static class DefaultPrecedenceEvaluator implements PrecedenceEvaluator {
+
+    private final Map<Operator, Integer> precedence;
+
+    private DefaultPrecedenceEvaluator(Map<Operator, Integer> precedence) {
+      this.precedence = precedence;
+    }
+
+    public static class PrecedenceEvaluatorBuilder {
+
+      private final Map<Operator, Integer> precedence = new HashMap<>();
+
+      public PrecedenceEvaluatorBuilder withOperatorPrecedence(Operator operator, Integer precedence) {
+        this.precedence.put(operator, precedence);
+        return this;
+      }
+
+      public PrecedenceEvaluator build() {
+        return new DefaultPrecedenceEvaluator(precedence);
+      }
+
+    }
+
+    @Override
+    public int compare(Operator o1, Operator o2) {
+      return Integer.compare(precedence.get(o1), precedence.get(o2));
+    }
+  }
+
+  public static class Calculator {
+
+    private final Stack<Double> nums;
+    private final Stack<Operator> ops;
+    private final PrecedenceEvaluator precedenceEvaluator;
+
+    public Calculator() {
+      this(new PrecedenceEvaluatorBuilder().withOperatorPrecedence(Operator.PLUS, 1)
+             .withOperatorPrecedence(Operator.MINUS, 1)
+             .withOperatorPrecedence(Operator.MULTIPLY, 2)
+             .withOperatorPrecedence(Operator.DIVIDE, 2)
+             .withOperatorPrecedence(Operator.POW, 3)
+             .withOperatorPrecedence(Operator.LP, 4)
+             .withOperatorPrecedence(Operator.RP, 4)
+             .build());
+    }
+
+    public Calculator(PrecedenceEvaluator precedenceEvaluator) {
+      nums = new Stack<>();
+      ops = new Stack<>();
+      this.precedenceEvaluator = precedenceEvaluator;
+    }
+
+    public long evaluate(List<String> lines) {
+      return lines.stream().map(expression -> (long) evaluate(expression)).reduce(0L, Long::sum);
+    }
+
+    public boolean isNumber(String str) {
+      try {
+        Double.parseDouble(str);
+      } catch (NumberFormatException | NullPointerException e) {
+        return false;
+      }
+      return true;
+    }
+
+    public double evaluate(String str) {
+      str = str.replaceAll("\\(", "( ").replaceAll("\\)", " )");
+      String[] tokens = str.split(" ");
+
+      for (String token : tokens) {
+        if (isNumber(token)) {
+          nums.push(Double.parseDouble(token));
+        } else if (token.equals(")")) {
+          while (ops.peek() != Operator.LP) {
+            eval();
+          }
+          ops.pop();
+        } else {
+          Operator operator = Operator.from(token.charAt(0));
+          if (ops.isEmpty()) {
+            ops.push(operator);
+          } else if (precedenceEvaluator.compare(operator, ops.peek()) > 0) {
+            ops.push(operator);
+          } else if (precedenceEvaluator.compare(operator, ops.peek()) <= 0) {
+            if (ops.peek() != Operator.LP) {
+              eval();
+            }
+            ops.push(operator);
+          }
+        }
+      }
+
+      while (!ops.isEmpty() && nums.size() > 1) {
+        eval();
+      }
+      assert nums.size() == 1;
+      return nums.pop();
+    }
+
+    private void eval() {
+      double second = nums.pop();
+      double first = nums.pop();
+      nums.add(ops.pop().getOperation().apply(first, second));
+    }
+
+  }
+
+  enum Operator {
+    PLUS('+', Double::sum),
+    MINUS('-', (a, b) -> a - b),
+    MULTIPLY('*', (a, b) -> a * b),
+    DIVIDE('/', (a, b) -> a / b),
+    POW('^', Math::pow),
+    LP('(', (a, b) -> {
+      throw new IllegalArgumentException("Not a valid operator");
+    }),
+    RP(')', (a, b) -> {
+      throw new IllegalArgumentException("Not a valid operator");
+    });
+
+    private static final Map<Character, Operator> symbolToOperator;
+
+    static {
+      symbolToOperator = Arrays.stream(Operator.values()).collect(Collectors.toMap(Operator::getSymbol, Function.identity()));
+    }
+
+    private final char symbol;
+    private final BiFunction<Double, Double, Double> operation;
+
+    Operator(char symbol, BiFunction<Double, Double, Double> operation) {
+      this.symbol = symbol;
+      this.operation = operation;
+    }
+
+    public static Operator from(char symbol) {
+      return symbolToOperator.get(symbol);
+    }
+
+    public BiFunction<Double, Double, Double> getOperation() {
+      return operation;
+    }
+
+    public char getSymbol() {
+      return symbol;
+    }
   }
 
 }
--- java/src/test/java/solutions2020/Solution18Test.java
+++ java/src/test/java/solutions2020/Solution18Test.java
@@ -2,6 +2,12 @@ package solutions2020;
 
 import org.junit.Assert;
 import org.junit.Test;
+import solutions2020.Solution18.Calculator;
+import solutions2020.Solution18.DefaultPrecedenceEvaluator.PrecedenceEvaluatorBuilder;
+import solutions2020.Solution18.Operator;
+import solutions2020.Solution18.ParserCalculator;
+import solutions2020.Solution18.PatternMatchingCalculator;
+import solutions2020.Solution18.PrecedenceEvaluator;
 import utils.FileReader;
 
 /**
@@ -11,43 +17,86 @@ public class Solution18Test {
 
   @Test
   public void testPartOne() {
-    Assert.assertEquals(6923486965641L, Solution18.partOne(FileReader.readAllLines("2020/18-input.txt")));
+    Assert.assertEquals(6923486965641L, new PatternMatchingCalculator().partOne(FileReader.readAllLines("2020/18-input.txt")));
   }
 
   @Test
   public void testPartOneSample() {
-    Assert.assertEquals(71 + 51 + 26 + 437 + 12240 + 13632, Solution18.partOne(FileReader.readAllLines("2020/18-sample.txt")));
-  }
-
-  @Test
-  public void testPartOneRec() {
-    Assert.assertEquals(6923486965641L, Solution18.partOneRec(FileReader.readAllLines("2020/18-input.txt")));
-  }
-
-  @Test
-  public void testPartOneSampleRec() {
-    Assert.assertEquals(71 + 51 + 26 + 437 + 12240 + 13632, Solution18.partOneRec(FileReader.readAllLines("2020/18-sample.txt")));
+    Assert.assertEquals(71 + 51 + 26 + 437 + 12240 + 13632,
+      new PatternMatchingCalculator().partOne(FileReader.readAllLines("2020/18-sample.txt")));
   }
 
   @Test
   public void testPartTwo() {
-    Assert.assertEquals(70722650566361L, Solution18.partTwo(FileReader.readAllLines("2020/18-input.txt")));
+    Assert.assertEquals(70722650566361L, new PatternMatchingCalculator().partTwo(FileReader.readAllLines("2020/18-input.txt")));
   }
 
   @Test
   public void testPartTwoSample() {
-    Assert.assertEquals(231 + 51 + 46 + 1445 + 669060 + 23340, Solution18.partTwo(FileReader.readAllLines("2020/18-sample.txt")));
+    Assert.assertEquals(231 + 51 + 46 + 1445 + 669060 + 23340,
+      new PatternMatchingCalculator().partTwo(FileReader.readAllLines("2020/18-sample.txt")));
+  }
+
+  @Test
+  public void testPartOneRec() {
+    Assert.assertEquals(6923486965641L, new ParserCalculator().partOne(FileReader.readAllLines("2020/18-input.txt")));
+  }
+
+  @Test
+  public void testPartOneSampleRec() {
+    Assert.assertEquals(71 + 51 + 26 + 437 + 12240 + 13632,
+      new ParserCalculator().partOne(FileReader.readAllLines("2020/18-sample.txt")));
   }
 
   @Test
   public void testPartTwoRec() {
-    Assert.assertEquals(70722650566361L, Solution18.partTwoRec(FileReader.readAllLines("2020/18-input.txt")));
+    Assert.assertEquals(70722650566361L, new ParserCalculator().partTwo(FileReader.readAllLines("2020/18-input.txt")));
   }
 
   @Test
   public void testPartTwoSampleRec() {
     Assert.assertEquals(231 + 51 + 46 + 1445 + 669060 + 23340,
-      Solution18.partTwoRec(FileReader.readAllLines("2020/18-sample.txt")));
+      new ParserCalculator().partTwo(FileReader.readAllLines("2020/18-sample.txt")));
+  }
+
+  @Test
+  public void testPartOneCalculator() {
+    Assert.assertEquals(6923486965641L,
+      new Calculator(getSamePrecedenceEvaluator()).evaluate(FileReader.readAllLines("2020/18-input.txt")));
+  }
+
+  @Test
+  public void testPartOneSampleCalculator() {
+    Assert.assertEquals(71 + 51 + 26 + 437 + 12240 + 13632,
+      new Calculator(getSamePrecedenceEvaluator()).evaluate(FileReader.readAllLines("2020/18-sample.txt")));
+  }
+
+  @Test
+  public void testPartTwoSampleCalculator() {
+    Assert.assertEquals(231 + 51 + 46 + 1445 + 669060 + 23340,
+      new Calculator(getInversePrecedenceEvaluator()).evaluate(FileReader.readAllLines("2020/18-sample.txt")));
+  }
+
+  @Test
+  public void testPartTwoCalculator() {
+    Assert.assertEquals(70722650566361L,
+      new Calculator(getInversePrecedenceEvaluator()).evaluate(FileReader.readAllLines("2020/18-input.txt")));
+  }
+
+  private PrecedenceEvaluator getSamePrecedenceEvaluator() {
+    return new PrecedenceEvaluatorBuilder().withOperatorPrecedence(Operator.PLUS, 1)
+             .withOperatorPrecedence(Operator.MULTIPLY, 1)
+             .withOperatorPrecedence(Operator.LP, 4)
+             .withOperatorPrecedence(Operator.RP, 4)
+             .build();
+  }
+
+  private PrecedenceEvaluator getInversePrecedenceEvaluator() {
+    return new PrecedenceEvaluatorBuilder().withOperatorPrecedence(Operator.PLUS, 2)
+             .withOperatorPrecedence(Operator.MULTIPLY, 1)
+             .withOperatorPrecedence(Operator.LP, 4)
+             .withOperatorPrecedence(Operator.RP, 4)
+             .build();
   }
 
 }
